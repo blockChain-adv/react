7fd3eb36b621e447b94a103416ef14bdb1fbab9b
thayoon <thagyun@naver.com>
2022-06-09 04:41:41 +0900

    Style: path/component create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import AdvertiserPage from "./pages/AdvertiserPage";
+import MyPage from "./pages/MyPage";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgba(215, 194, 242, 0.3);
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route element={<MainPage />} path="/" />
+        <Route element={<AdvertiserPage />} path="/Advertiser" />
+        <Route element={<MyPage />} path="/MyPage" />
+      </Routes>
+    </>
   );
 }
 
